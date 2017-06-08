d2215a508af33828870f1d64825155d2ece01492
Flora Carey <careyf@reed.edu>
2017-06-07 20:54:39 -0700

    information can now be printed to .txt file

--- templates/app.py
+++ templates/app.py
@@ -9,28 +9,44 @@ stripe_keys = {
 
 stripe.api_key = stripe_keys['secret_key']
 
-
 app = Flask(__name__)
+
+# Token is created using Stripe.js or Checkout!
+# Get the payment token submitted by the form:
+# token = POST['stripeToken'] # Using Flask
+
 
 @app.route('/')
 def index():
     return render_template('index.html',key=stripe_keys['publishable_key'])
 @app.route('/charge', methods=['POST'])
 def charge():
-    # amt in cents
-    amount = 500
+  amount=999
+  #amt in cents
+# Create a Customer:
+  customer = stripe.Customer.create(
+    email="paying.user@example.com",
+    source=request.form['stripeToken']
+    )
 
-    customer = stripe.Customer.create(
-        email = 'customer@example.com',
-        source=request.form['stripeToken']
-        )
-    charge = stripe.Charge.create(
-        customer=customer.id,
-        amount=amount,
-        currency='usd',
-        description = 'Flask Charge'
-        )
-    return render_template('charge.html',amount=amount)
+# Charge the user's card:
+  charge = stripe.Charge.create(
+    customer=customer.id,
+    amount=amount,
+    currency='usd',
+    description="ReeDistribute Charge"
+  )
+  with open("database3.txt","w+") as f:
+      f.write("Customer ID: " + str(customer.id) + '\n' + "Amount Charged (cents): " + str(charge.amount)
+       +'\n' + "email: " + str(customer.email))
+      print(f)
+      #this prints any information we may want to store onto a .txt file
+      #right now, customer-id,amount,email
+  
+  return render_template('charge.html',amount=amount)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
