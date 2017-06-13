e1233994533272912142312f2594762c980ee6e4
HannahSMead <Meadsha@reed.edu>
2017-06-13 16:01:40 -0700

    Added requests, removed login
    Since I am doing page by page authorization, we no longer nee a log in page. I also added a cool bar at the top

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import os
 from flask import Flask, render_template, request
 import stripe
 
-from controls import create_student, create_donor, authenticate, donate
+from controls import create_student, create_donor, open_request, authenticate, get_id, get_student_id, get_donor_id
 import schema
 import db
 
@@ -47,6 +47,7 @@ def submit_donation():
     passwordAttempt=request.form['psw']
     result = authenticate(usernameAttempt, passwordAttempt)
     if result: 
+
         return render_template('LSESdonation.html')
     else: return render_template('loginFailure.html')
     
@@ -110,6 +111,23 @@ def charge():
       #right now, customer-id,amount,email
   
   return render_template('charge.html',amount=amount)
+
+@app.route('/open_request')
+def make_request():
+    return render_template('request.html')
+
+@app.route('/submit-request', methods=['POST'])
+def check_request():
+    requiredmoney = request.form['amt']
+    reason = request.form['dscrp']
+    usernameAttempt = request.form['uname']
+    passwordAttempt = request.form['psw']
+    result = authenticate(usernameAttempt, passwordAttempt)
+    if result: 
+        ID = get_id(usernameAttempt)
+        open_request(ID, amt, reason)
+        return render_template('loginsuccess.html')
+    else: return render_template("loginFailure.html") 
 
 if __name__ == '__main__':
     app.run(debug=True)
