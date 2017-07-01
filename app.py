b1953d3f2265f0a3e58dc8c4cf308c785b0d9809
Flora Carey <careyf@reed.edu>
2017-06-30 19:06:54 -0700

    example request format
    homepage now shows requests (but only example)

--- app.py
+++ app.py
@@ -1,144 +1,153 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+{% extends "basic.html" %}
+{% block content %}
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ReeDistribute</title>
+    <style type="text/css" media="screen">
+  form {
+      padding-left:25%;
+      padding-right:25%;
+  }
+  button.accordion {
+    background-color: #eee;
+    color: #444;
+    cursor: pointer;
+    padding: 16px;
+    width: 50%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+    transition: 0.4s;
+  }
 
-from flask import Flask, render_template, request, jsonify, templating
-import stripe
+  button.accordion.active, button.accordion:hover {
+      background-color: #ddd;
+  }
 
-from controls import (create_student, create_donor, open_request,
-                        authenticate, get_id, get_student_id,
-                        get_donor_id, request_info, update_account_token)
-import schema
-import db
-from config import app, stripe_keys, api_key
-"""
-from config import POSTS_PER_PAGE, MAX_SEARCH_RESULTS...
-"""
+  button.accordion:after {
+      content: '\002B';
+      color: #777;
+      font-weight: bold;
+      float: right;
+      margin-left: 5px;
+  }
 
+  button.accordion.active:after {
+      content: "\2212";
+  }
 
+  div.panel {
+      padding: 0 18px;
+      background-color: white;
+      max-height: 0;
+      overflow: hidden;
+      transition: max-height 0.2s ease-out;
+  }
+  body {
+    background-color: #ffda56;
+    margin: 1;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    padding: 1.5rem;
+    background-color: #efefef;
+    text-align: center;
+    color: #9b0700;
+  }
+    #when {
+      font-size:11px;
+    }
+    #entry {
+      background-color: #efefef;
+      border: 3px solid #f1f1f1;
+      color: black;
+    }
+    #who {
+      font-size:11px;
+      color:black;
+    }
+    #uuid, #uuid2 {
+      visibility: hidden;
+    }
+    h2 {
+      text-align: center;
+      float:center;
+    }
+    h4 { 
+      display:inline;
+      text-align: left
+      color:#9b0700;
+    }
 
-@app.route("/", methods=["POST"])
-def main():
-    return render_template("basic.html")
+</style>
+</head>
+<p>
+<?xml version="1.0" encoding="utf-8"?>
+<body>
+<feed xmlns="http://www.w3.org/2005/Atom">
+<h2>Example Feed</h2>
+<button class="accordion"><post-title style="font-size:20px"> I need this </post-title><br><author class="who" style="font-size: 11px">John Doe </author></button>
+  <div class="panel">
+  <br>
+  <updated id="when">2003-12-13T18:30:02</updated>
+  <br>
+  <id id="uuid">urn:uuid:60a76c80-d399-11d9-b93C-0003939e0af6</id>
+  <br>
+    <entry id="entry" type="container" style="background-color: #efefef; display:inline-block; margin:auto; text-align: center;">
+      <span name="post">One Example Request</span> <br>
+      <!-- post name will be referencing the database -->
 
-@app.route("/", methods=["GET"])
-def make_cards():
-    # Front page, displays the request cards
-    # If there is a better way to do this, please show me -H
-    # start = '{% extends "basic.html" %} {% block content %} '
-    # card = '<div class="card"><div class="container"><h4><b> %s, </b> %s</h4><p>$ %d</p><div id="myProgress"><div id="myBar" style="width: %f">%f</div></div> <p> %s </p><a href="/log_donation/%d"> HELP OUT</a></div></div>'
-    # end = '{% endblock %}'
-    # allCards = ''
-    info = request_info()
-    for request in info:
-        percent_filled = (request[2]/request[1])*100
-        if percent_filled >= 100: percent_filled = 100
-        card_data = info
-    return render_template("basic.html")
+      <id id=uuid2>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id>
+      <!-- this will be referencing the ID number, hidden right now -->
+      <span class="desc" style="float:center;">Some text.</summary>
+      <!-- database's description text. Should we create a character limit for this? (yes) (like 1000 words right?) -->
+      <form action="/charge" method="POST">
+      <!-- this charges the customer -->
+      <span class="amount" style="display:block;">$<input type="value" class="amount" required></span>
+      <br>
+      <h4>
+      <form action="/charge" method="POST">
+      <script
+      src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+      data-key="pk_test_KovI8IiKzzuFbk33ewAjbYzc" 
+      data-name="example rqst"
+      data-image="https://www.hscripts.com/freeimages/logos/academic-institution-logos/reed-college-griffin/reed-college-griffin-256.gif"
+      data-locale="auto">
+      </script>
+      <!-- stripe API, data-name and data-desc should be a variable -->
+      </form>
+      </h4>
+      </form>
+    </entry>
+</feed>
+</p>
+</div>
+</body>
 
+</div>
+<script>
+var amt_donate = document.getElementsByClassName("amount");
+var desc = document.getElementsByClassName("desc");
+var author = document.getElementsByClassName("who");
+// make the amount, desc, and author variables-- right now it's examples (db)
 
-@app.route("/log_donation/<ID>", methods=["POST", "GET"])
-@app.route("/log_donation")
-def get_donation_info(ID=None):
-    if ID: message = "For Request # %s" %(ID)
-    else: message = "ignor this page" 
-    return render_template("donation.html", info=message)
-
-    
-@app.route("/create_account")
-def create_account():
-    return render_template("addform.html")
-
-
-@app.route("/record_account", methods=["POST"])
-def add_account():
-    username=request.form["username"]
-    name =request.form["name"]
-    email=request.form["email"]
-    pw1 =request.form["pw1"]
-    pw2 =request.form["pw2"]
-    
-
-    if (pw1 == pw2):
-        create_student(username, name, pw1, email)
-        return render_template("frontpage.html") 
-    else: 
-        return render_template("addform.html")
-
-
-@app.route("/login")
-# Since we as of now are not implementing user sessions, this is not needed
-def log_in():
-    return render_template("login.html")
-
-
-@app.route("/are-you-real", methods=["POST"])
-def check_credentials():
-    usernameAttempt = request.form["uname"]
-    passwordAttempt = request.form["psw"]
-    result = authenticate(usernameAttempt, passwordAttempt)
-    return render_template("basic.html")
-    # if result: 
-    #     return render_template("loginsuccess.html")
-    # else: 
-    #     return render_template("loginFailure.html")
-
-
-@app.route("/pay", methods=["POST"])
-def index():
-    return render_template("index.html",key=stripe_keys["publishable_key"])
-
-@app.route("/charge", methods=["POST"])
-def charge():
-    howMuch=request.form["amount"]
-    customer = stripe.Customer.create(
-        source=request.form["stripeToken"],
-        email="paying.user@example.com"
-    )
-
-    charge = stripe.Charge.create(
-        customer=customer.id,
-        amount=howMuch,
-        currency="usd",
-        description="Donation"
-    )
-
-    return render_template('charge.html', amount=howMuch)
-
-
-@app.route("/open_request")
-def make_request():
-    return render_template("request.html")
-
-@app.route("/submit-request", methods=["POST"])
-def check_request():
-    requiredmoney = request.form["amount"]
-    reason = request.form["dscrp"]
-    usernameAttempt = request.form["uname"]
-    passwordAttempt = request.form["psw"]
-    # result = authenticate(usernameAttempt, passwordAttempt)
-    ID = get_id(usernameAttempt)
-    open_request(ID,requiredmoney,reason)
-    return render_template("submit-request.html")
-    # if result: 
-    #     ID = get_id(usernameAttempt)
-    #     open_request(ID, amt, reason)
-    #     return render_template("loginsuccess.html")
-    # else: 
-    #     return render_template("loginFailure.html") 
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
-    
-
-if __name__ == "__main__":
-    schema.start_db()
-    
-    app.run(debug=True)
+</script>
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+</script>
+{% endblock %}
