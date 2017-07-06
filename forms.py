a8ff9c609e689538c45492330c805f887f1fd5ce
Flora Carey <careyf@reed.edu>
2017-07-06 14:44:48 -0700

    deleted commented out code

--- forms.py
+++ forms.py
@@ -1,4 +1,3 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
 from controls import request_info, request_info_who
 import json
 class PostForm():
@@ -8,13 +7,3 @@ class PostForm():
 	leng = len(info[1])
 	def ListIt(self,post_n,type_n):
 		return request_info_who(post_n,type_n)
-			# amount_needed = i[1]
-			# amount_given = i[2]
-			# title = i[3]
-			# desc = i[4]
-			# req_id = i[5]
-
-
-
-	# i = json.dumps(info)[1]
-# class GetInfo(PostForm)
