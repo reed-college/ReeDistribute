7ec1050ca7f60e94f2661b0e8e03369de2fd6bab
Flora Carey <careyf@reed.edu>
2017-07-25 14:20:01 -0400

    getlength function added

--- forms.py
+++ forms.py
@@ -4,6 +4,7 @@ class PostForm():
 	info = request_info()
 	print(info)
 	leng_2 = len(info)
-	leng = len(info[1])
 	def ListIt(self,post_n,type_n):
 		return request_info_who(post_n,type_n)
+	def GetLength(self):
+		return self.leng_2
