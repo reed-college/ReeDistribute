1d764a061fe75326f67ed60997cb9c09e32dea91
HannahSMead <Meadsha@reed.edu>
2017-07-31 14:06:50 -0700

    Removed Excess Storing
    After finding ourselves using single sign on I needed to update the database so we can hold less information as accounts are already stored.

--- controls.py
+++ controls.py
@@ -5,33 +5,30 @@ start_db()
 
 
 ######################################Creation Functions
-def create_student(username, name, password, email, token=""):
+def create_account(username, admin, approved, token = ''):
     # Create a Student and Account instance in the rd database.
     s = db.get_session()
 
-    Acc =  Account(username=username, name=name, password=password, email=email, account_token=token)
+    Acc =  Account(username=username, admin=admin, approved=approved, account_token=token)
     s.add(Acc)
-    s.flush()
-
-    Stu = Student(related_account=Acc.id)
-    s.add(Stu)
     s.commit()
     s.close()
+
     print(username, "added to the database")
 
-def create_donor(username, password, email, token=""):
-    # Create a Donor and Account instance in the rd database
+def approve_admin(username):
     s = db.get_session()
-
-    Acc =  Account(username=username, password=password, email=email, account_token=token)
-    s.add(Acc)
-    s.flush()
-
-    Don = Donor(related_account=Acc.id)
-    s.add(Don)
+    A = s.query(Account).filter(Account.username==usernameAttempt).first()
+    A.admin = True
     s.commit()
     s.close()
 
+def approve_requesting(username):
+    s = db.get_session()
+    A = s.query(Account).filter(Account.username==usernameAttempt).first()
+    A.approved = True
+    s.commit()
+    s.close()
 
 def open_request(student_id, amount, reason, title="Donate to a Reedie in needie", anon=True, amount_filled=0.0):
     # Create a request instance in the rd database.
@@ -43,26 +40,22 @@ def open_request(student_id, amount, reason, title="Donate to a Reedie in needie
     print(student_id, "request added to the database")
        
 
-def donate(request_id, donor_id, amount, account_token):
+def donate(username, request_id, amount):
     # Create and log the effects of a Donation in the database
     s = db.get_session()
-    D = s.query(Donor).get(donor_id)
-    A = s.query(Account).get(D.related_account)
-    R = s.query(Request).get(request_id)
+    A = s.query(Account).filter(Account.username==usernameAttempt).first()
+    R = s.query(Request).filter(request.id==request_id).first()
 
-
-    A.account_token = account_token
-    D.money_given += amount
-    D.donations_given += 1
+    A.given += amount
+    A.num_given += 1
+    s.flush()
     R.amount_filled += amount
 
     # check to see if the request has been completed
     if R.amount_needed >= R.amount_filled:
         R.filled = True
 
-    d = Donation(to_request=request_id, from_donor=donor_id,amount_given = amount)
 
-    s.add(d)
     s.commit()
     s.close()
 
@@ -78,41 +71,6 @@ def get_id(usernameAttempt):
     return A.id
 
 
-def get_student_id(usernameAttempt):
-    # Return the database Student id for an account from the username
-    s = db.get_session()
-    A = s.query(Account).filter(Account.username==usernameAttempt).first()
-    
-    acc = A.id
-    S = s.query(Student).filter(Student.related_account==acc).first()
-    
-    s.close()
-    return S.id
-
-def get_student_name(studentID):
-    # Return the database Student id for an account from the username
-    s = db.get_session()
-   
-    S = s.query(Student).get(studentID)
-    accountID = S.related_account
-   
-    A = s.query(Account).get(accountID)
-    who = A.name
-   
-    s.close()
-    return who
-
-
-def get_donor_id(usernameAttempt):
-    # Return the database Donor id for an account from the username
-    s = db.get_session()
-   
-    A = s.query(Account).filter(Account.username==usernameAttempt).first()
-    acc = A.id
-    D = s.query(Donor).filter(Donor.related_account==acc).first()
-   
-    s.close()
-    return D.id
 
 def update_account_token(username, token):
     s = db.get_session()
@@ -155,35 +113,6 @@ def request_info_who(post_n,type_n):
     return data
 # print(request_info()[1][0])
 
-def authenticate(usernameAttempt, passwordAttempt):
-    # Check to see if a user is in the database
-    # returns True/False
-    s = db.get_session()
-    
-    A = s.query(Account).filter(Account.username==usernameAttempt).first()
-    if A is None:
-        ret = False
-    elif (A.password == passwordAttempt): 
-        ret = True
-    else:
-        ret = False
-   
-    s.close()
-    return ret
-########################################################################## Untested
-# def update_password(userName, oldPass, newPass1, newPass2):
-#     s = db.get_session()
-#     A = s.query(Account).filter(Account.username==userName).first()
-#     if A is None: 
-#         Return False
-#     elif A.password == oldPass:
-#         if newPass1 == newPass2:
-#             A.password = newPass1
-#             return True
-#         return False
-#     return False
-#     s.commit()
-#     s.close()
 
 def approve_request(id):
     s = db.get_session()
@@ -196,69 +125,27 @@ def approve_request(id):
 
 ######################################################################################## TESTS
 
-def test_donation():
-    # Tests the donate function
-    create_student("Peachy","Georgia", "Pe@ch3s","On@my.mind")
-    create_donor("Mr.Money-Bags", "Ka-CH1NG!","monopolywinner@funny.money")
-    open_request(1,34,"I need money for Rent")
-    donate(3,2,15.0, "token")
-# test_donation()
-    
-
-
-def test_tables():
-    # Test table creation and insertion
-    # Check Creation
-    S = create_student("rubiesandemralds","jolene", "mine", "EMAIL" )
-    S = create_student("KIKI","Kendra", "WITCH", "EMAIL" )
-    S = create_student("Moonshine","Jordan", "brewer", "EMAIL" )
-    D = create_donor("bananabread", "MONEY", "MAIL")
-    D = create_donor("moneybagz", "richy", "MAIL")
-    D = create_donor("Curtis", "Day", "MAIL")
-    
-
-    # print(get_id("rubiesandemralds"), get_id("bananabread"))
-
-# test_tables()
-
-
-def test_authentication():
-    # Test a few cases to see if authentication works
-    A = Account(username = "LonelyLady",name = "Eleanor Rigby", password="ringoSTA**",email = "doIbelong@here.where",account_token = "MCKENZIE")
+def test_accounts():
     s = db.get_session()
+    A = Account(username='HannahBanana')
+    B = Account(username='DarkLordVold')
+    C = Account(username='Krogerdile')
     s.add(A)
+    s.add(B)
+    s.add(C)
     s.commit()
-    r1 = authenticate("LonelyLady", "ringoSTA**")
-    r2 = authenticate("LonelyLaddie", "ringoSTA**")
-    r3 = authenticate("LonelyLady", "YOKO")
     s.close()
-    print(r1, r2, r3)
-# test_authentication()
-def test_approval():
-    approve_request(2)
-# test_approval()
 
 def test_requests():
-    # Test to see if we can return all the request info needed for posting
-    create_student("bananana","HANNAH","phone", "EMAIL@MAILMAIL.MAIL")
-    open_request(1, 55, "WOOOOOOOOOOOOOOOOOOW", False)
-    open_request(1, 22, "I need more cake, it was a lie")
-    # print(request_info())
-# test_requests()
-    
-    
-def TEST():
-    start_db()
-    test_tables()
-    open_request(1, 60.00, "I NEED A TOAD. TRUST ME.", "TOADZ")
-    donate(1, 1, 5.35, "kEYKey")
-    # test_donation()
     s = db.get_session()
-
-    for row in s.query(Account):
-        print(row)
-    for row in s.query(Request):
-        print(row)
-
+    R1 = Request(requested_by=1, amount_needed=20.87, title="For Books",description="I would like to but the LOTR trillogy in paperback")
+    R2 = Request(requested_by=2,amount_needed=6.66, title='Horcrux', description='keep me alive forever')
+    s.add(R1)
+    s.add(R2)
+    s.commit()
     s.close()
+
+def TEST():
+    test_accounts()
+    test_requests()
 # TEST()
