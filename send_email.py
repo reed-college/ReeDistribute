d0b02481ec35a87061497be8a0775b6415e44d0f
HannahSMead <Meadsha@reed.edu>
2017-08-12 17:59:27 -0700

    Html Email sender
    This email sender can be formatted with html!

--- send_email.py
+++ send_email.py
@@ -11,14 +11,21 @@ def activation_email(to,code):
     msg['Subject'] = "ReeDistribute Activation Code"
     url = "reed.edu/reedistribute"
 
-    body = "Welcome! You are invited to join ReeDistribute, an online communtiy for Reed College student monetary gifting!"
-    body += "\n"
-    body += "To activate you account please go to " + url + " and use this email address along with the following activation code: " + code
-    msg.attach(MIMEText(body,'plain'))
-
+    html = """\
+    <html>
+    <head></head>
+    <body style='text-align:center'>
+    <h1> Welcome! </h1>
+    <h3> You are invited to join ReeDistribute, an online communtiy for Reed College student monetary gifting! </h3>
+    <h2> To activate you account please go to %s and use this email address along with the following activation code: %s
+    </body>
+    </html>
+    """ %(url, code)
+    msg.attach(MIMEText(html,'html'))
     server = smtplib.SMTP("smtp.gmail.com",587)
     server.starttls()
     server.login(fro,"pipinstallreed")
     text = msg.as_string()
     server.sendmail(fro,to,text)
     server.quit()
+activation_email("meadsha@reed.edu","WOW")
